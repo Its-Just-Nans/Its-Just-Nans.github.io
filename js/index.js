145088374cc7beaadd68c98703078d4e03de1a0e
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2020-11-11 18:15:30 +0100

    [Feature] add

--- js/index.js
+++ js/index.js
@@ -1,20 +1,9 @@
-function copy(param){
-    let copieur = document.createElement('textarea');
-    copieur.value = param;
-    copieur.innerHTML = param;
-    copieur.style.width = '1px';
-    copieur.style.height = '1px';
-    document.body.appendChild(copieur);
-    copieur.focus();
-    copieur.select();
-    document.execCommand('copy');
-    try{window.getSelection().removeAllRanges();}catch(e){}
-}
+
 
 function render(projects){
     document.getElementById('projects').innerHTML = '';
     //utilisation du json
-    let toDelete;
+    /*let toDelete;
     Object.keys(projects).forEach(function(key) {
         if(projects[key].name == 'Its-Just-Nans.github.io'){
             toDelete = key;
@@ -22,13 +11,13 @@ function render(projects){
     });
     if(toDelete){
         projects.splice(toDelete, 1);
-    }
+    }*/
     for(element of projects){
         let part = document.createElement('div');
         part.className = 'projectsClass';
         let title = document.createElement('h4');
         let link = document.createElement('a');
-        link.href = '/'+element['name'];
+        link.href = './'+element['name'];
         link.className = 'full';
         link.className += ' black';
         link.className += ' nothing';
@@ -44,12 +33,10 @@ var goodData;
 let client = new HttpClient();
 client.get('https://its-just-nans.github.io/projects.json', function(response) {
     const backUpJSON = JSON.parse(response);
-    setTimeout( ()=>{
         if(document.getElementById('projects').innerHTML === ''){
             let tab = backUpJSON.slice();
             render(tab);
         }
-    }, 200);
     let client2 = new HttpClient();
     client2.get('https://api.github.com/users/Its-Just-Nans/repos', function(responseAPI) {
         console.log('✔️:responseAPI');
