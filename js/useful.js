0ee7e02b643e63530d65fc53d29f87940638792c
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-01-10 18:00:03 +0100

    [fix] add function

--- js/useful.js
+++ js/useful.js
@@ -24,6 +24,30 @@ var HttpClient = function() {
     }
 }
 
+function makeRequest (method, url) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = function () {
+        if (this.status >= 200 && this.status < 300) {
+          resolve(xhr.response);
+        } else {
+          reject({
+            status: this.status,
+            statusText: xhr.statusText
+          });
+        }
+      };
+      xhr.onerror = function () {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      };
+      xhr.send();
+    });
+  }
+
 function copy(param){
     let copieur = document.createElement('textarea');
     copieur.value = param;
@@ -37,7 +61,14 @@ function copy(param){
     try{
         window.getSelection().removeAllRanges();
     }catch(e){
-        console.log('Copy fail')
+        console.log('Copy failed')
     }
     copieur.remove();
 }
+
+function removeAccent(s){
+    var r = s.toLowerCase();
+    non_asciis = {'a': '[àáâãäå]', 'ae': 'æ', 'c': 'ç', 'e': '[èéêë]', 'i': '[ìíîï]', 'n': 'ñ', 'o': '[òóôõö]', 'oe': 'œ', 'u': '[ùúûűü]', 'y': '[ýÿ]'};
+    for (i in non_asciis) { r = r.replace(new RegExp(non_asciis[i], 'g'), i); }
+    return r;
+  }
