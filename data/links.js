6197358623cb2b9db45d8820d4ad810f4d2167ee
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-02-21 14:51:18 +0100

    [feature] add links parts

---
+++ data/links.js
@@ -0,0 +1,17 @@
+var links = [
+    {
+        ico: "https://github.githubassets.com/favicon.ico",
+        name: "GitHub",
+        link: "https://github.com/Its-Just-Nans"
+    },
+    {
+        ico: "https://abs.twimg.com/favicons/twitter.ico",
+        name: "Twitter",
+        link: "https://twitter.com/Its_Just_Nans"
+    },
+    {
+        ico: "https://www.sololearn.com/Images/favicon.ico",
+        name: "SoloLearn",
+        link: "https://www.sololearn.com/profile/7347864"
+    },
+];
--- js/index.js
+++ js/index.js
@@ -33,30 +33,83 @@ function render(projects){
 document.getElementById('frontTitle').innerHTML = document.title;
 
 var goodData;
-let client = new HttpClient();
-client.get('https://its-just-nans.github.io/projects.json', function(response) {
+let urlBackUp = window.location.origin + '/data/projects.json';
+// we use window.location.origin to use the site in local
+
+/*
+    We first request the backup Json (we can also GET that backup file with a script tag, like links.js), but this time I choose this method
+    After, we use the GitHub API to check is there are any new projects (and re-render the projects part if so)
+*/
+makeRequest('GET', urlBackUp).then( function(response){
     const backUpJSON = JSON.parse(response);
         if(document.getElementById('projects').innerHTML === ''){
             let tab = backUpJSON.slice();
             render(tab);
         }
     let client2 = new HttpClient();
-    client2.get('https://api.github.com/users/Its-Just-Nans/repos', function(responseAPI) {
+    makeRequest('GET', 'https://api.github.com/users/Its-Just-Nans/repos').then( function(responseAPI){
         console.log('✔️:responseAPI');
         const apiJSON = JSON.parse(responseAPI);
         if(apiJSON.length != backUpJSON.length){
-            smollPopUp({title : 'Message to admin', content : 'curl.exe -o projects.json https://api.github.com/users/Its-Just-Nans/repos'}, 'ko', function Copier(rep){copy(rep.content)})
+            smollPopUp({title : 'Message to admin', content : 'curl.exe -o data/projects.json https://api.github.com/users/Its-Just-Nans/repos'}, 'ko', function Copier(rep){copy(rep.content)})
             goodData = apiJSON;
             let renderer = goodData;
             render(renderer);
             console.log('✔️:render with API data');
         }
-    }, function(error){
-        console.log('❌:responseAPI');
-        console.log(error);
+    }).catch(function (error){
+        console.log('log: ');
+        console.error(error);
     });
-}, function (error){
-    console.log('log: '+ error.toString());
+}).catch(function (error){
+    console.log('log: ');
+    console.error(error);
 });
 
-//use goodData as JSON
+
+if(links){
+    let tbody = document.getElementById('bodyLinks');
+    let renderColum = function(row, link, content, className){
+        let column = document.createElement('td');
+        if(className){
+            column.className = className;
+        }
+        if(link){
+            let oneLink = document.createElement('a');
+            oneLink.innerHTML = content;
+            oneLink.href = link;
+            oneLink.target = "_blank";
+            column.appendChild(oneLink);
+        }else{
+            column.innerHTML = content;
+        }
+        row.appendChild(column);
+        return column;
+    };
+    for(let oneElement of links){
+        let row = document.createElement('tr');
+        renderColum(row, oneElement.link, `<img src="${oneElement.ico}" class="iconSmall">`, null);
+        renderColum(row, oneElement.link, oneElement.name, null);
+        renderColum(row, oneElement.link, oneElement.link, "wordBreak");
+        //this ico is from BitBucket
+        let copySVG = `<svg width="24" height="24" viewBox="0 0 24 24" focusable="false" class="copyIcon"><g><path d="M10 19h8V8h-8v11zM8 7.992C8 6.892 8.902 6 10.009 6h7.982C19.101 6 20 6.893 20 7.992v11.016c0 1.1-.902 1.992-2.009 1.992H10.01A2.001 2.001 0 0 1 8 19.008V7.992z"></path><path d="M5 16V4.992C5 3.892 5.902 3 7.009 3H15v13H5zm2 0h8V5H7v11z"></path></g></svg>`;
+        let copyColumn = renderColum(row, null, copySVG);
+        copyColumn.onclick = function(){
+            let link = this.parentNode.children[0].children[0].href;
+            copy(link);
+            smollPopUp({title : "Copied", content : ''}, 'ok')
+        };
+        row.appendChild(copyColumn);
+        tbody.appendChild(row);
+        /*
+        row.onclick = function(event){
+            debugger
+            if(!event.target.href){
+                let page = row.children[0].children[0].href;
+                win = window.open(page, '_blank');
+                win.focus();
+            }
+        };
+        */
+    }
+}
