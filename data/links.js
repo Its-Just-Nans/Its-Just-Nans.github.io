ea0c1037b9e8b272cdef61c2bb23fc46765a75c2
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-03-07 14:03:00 +0100

    [fix] change logos

--- data/links.js
+++ data/links.js
@@ -1,31 +1,31 @@
 var links = [
     {
-        ico: "https://github.githubassets.com/favicon.ico",
+        ico: "./imgs/logos/github.png",
         name: "GitHub",
         link: "https://github.com/Its-Just-Nans"
     },
     {
-        ico: "https://static-exp1.licdn.com/sc/h/al2o9zrvru7aqj8e1x2rzsrca",
+        ico: "./imgs/logos/linkedin.png",
         name: "LinkedIn",
         link: "https://www.linkedin.com/in/nans-webert/"
     },
     {
-        ico: "https://abs.twimg.com/favicons/twitter.ico",
+        ico: "./imgs/logos/twitter.png",
         name: "Twitter",
         link: "https://twitter.com/Its_Just_Nans"
     },
     {
-        ico: "https://discord.com/assets/07dca80a102d4149e9736d4b162cff6f.ico",
+        ico: "./imgs/logos/discord.png",
         name: "Discord",
         link: "https://discord.gg/XsmsWvbdqv"
     },
     {
-        ico: "https://www.sololearn.com/Images/favicon.ico",
+        ico: "./imgs/logos/sololearn.png",
         name: "SoloLearn",
         link: "https://www.sololearn.com/profile/7347864"
     },
     {
-        ico: "https://cdn.arduino.cc/header-footer/prod/assets/favicon-arduino/favicon-32x32.png",
+        ico: "./imgs/logos/arduino.png",
         name: "Arduino Forums",
         link: "https://forum.arduino.cc/index.php?action=profile;u=1420654"
     }
