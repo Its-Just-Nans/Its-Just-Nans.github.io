24fb07953b2596669e1c6130293f17ab82a588db
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-03-14 14:41:03 +0100

    [fix] change script place

--- js/useful.js
+++ js/useful.js
@@ -1,80 +1,13 @@
-var HttpClient = function() {
-    this.get = function(aUrl, aCallBack, aErrorCallBack) {
-        try{
-            var anHttpRequest = new XMLHttpRequest();
-            anHttpRequest.onreadystatechange = function() { 
-                if (anHttpRequest.readyState == 4){
-                    if(anHttpRequest.status == 200){
-                        aCallBack(anHttpRequest.responseText);
-                    }else if(anHttpRequest.status == 403){
-                        aErrorCallBack(anHttpRequest.statusText);
-                    }else{
-                        aErrorCallBack();
-                    }
-                }
-            };
-            anHttpRequest.onerror = function(e){
-                aErrorCallBack(e);
-            };
-            anHttpRequest.open("GET", aUrl, true);     
-            anHttpRequest.send(null);
-        }catch(error){
-            aErrorCallBack(error);
-        }
-    }
-}
-
-function makeRequest (method, url) {
-    return new Promise(function (resolve, reject) {
-      var xhr = new XMLHttpRequest();
-      xhr.open(method, url);
-      xhr.onload = function () {
-        if (this.status >= 200 && this.status < 300) {
-          resolve(xhr.response);
-        } else {
-          reject({
-            status: this.status,
-            statusText: xhr.statusText
-          });
-        }
-      };
-      xhr.onerror = function () {
-        reject({
-          status: this.status,
-          statusText: xhr.statusText
+async function renderSpecialObject(name, callback) {
+    makeRequest("GET", `${urlToData}${name}`)
+        .then(function (responseAPI) {
+            console.log(`✔️${name}`);
+            const data = JSON.parse(responseAPI);
+            callback(data);
+            console.log(`⚙️${name}`);
+        })
+        .catch(function (error) {
+            console.error(`❌${name}`);
+            console.error(error);
         });
-      };
-      xhr.send();
-    });
-  }
-
-function copy(param){
-    let copieur = document.createElement('textarea');
-    copieur.value = param;
-    copieur.innerHTML = param;
-    copieur.style.width = '1px';
-    copieur.style.height = '1px';
-    document.body.appendChild(copieur);
-    copieur.focus();
-    copieur.select();
-    document.execCommand('copy');
-    try{
-        window.getSelection().removeAllRanges();
-    }catch(e){
-        console.log('Copy failed')
-    }
-    copieur.remove();
-}
-
-function removeAccent(s){
-    var r = s.toLowerCase();
-    non_asciis = {'a': '[àáâãäå]', 'ae': 'æ', 'c': 'ç', 'e': '[èéêë]', 'i': '[ìíîï]', 'n': 'ñ', 'o': '[òóôõö]', 'oe': 'œ', 'u': '[ùúûűü]', 'y': '[ýÿ]'};
-    for (i in non_asciis) { r = r.replace(new RegExp(non_asciis[i], 'g'), i); }
-    return r;
-}
-  
-function addStyle(HTMLelement, objectOfStyle){
-    for(let index in objectOfStyle){
-        HTMLelement.style[index] = objectOfStyle[index];
-    }
 }
