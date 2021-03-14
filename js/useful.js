bef913c10c56c91d1810b752c1e60842306a91e2
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-03-14 23:49:16 +0100

    [fix]

--- js/useful.js
+++ js/useful.js
@@ -1,4 +1,4 @@
-async function renderSpecialObject(params = { url, parse = true }, callback, errorCallBack = null) {
+async function renderSpecialObject(params = { url: url, parse: true }, callback, errorCallBack = null) {
     /*
         name: name of the file, ex :FILE.json
         callback: callBack to render with data
