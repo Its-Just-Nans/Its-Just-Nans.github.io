9689edb69380e102188098ba5b1077976c03256a
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-03-14 15:21:06 +0100

    [fix] add params

--- js/useful.js
+++ js/useful.js
@@ -1,8 +1,26 @@
-async function renderSpecialObject(name, callback) {
-    makeRequest("GET", `${urlToData}${name}`)
-        .then(function (responseAPI) {
+async function renderSpecialObject(name, callback, parse = true) {
+    /*
+        name: name of the file, ex :FILE.json
+        callback: callBack to render with data
+        parse: if we parse the response
+        pathToData: (optional) set link to data in global variable
+    */
+
+    let url;
+    if (name.endsWith(".json") && pathToData) {
+        url = `${pathToData}${name}`;
+    } else {
+        url = name;
+    }
+    makeRequest("GET", url)
+        .then(function (response) {
             console.log(`✔️${name}`);
-            const data = JSON.parse(responseAPI);
+            let data;
+            if (parse) {
+                data = JSON.parse(response);
+            } else {
+                data = response
+            }
             callback(data);
             console.log(`⚙️${name}`);
         })
