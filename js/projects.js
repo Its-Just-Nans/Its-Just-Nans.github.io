7690cb273714b208ae0057cfb787b86eb542279d
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-03-14 15:21:29 +0100

    [fix] make better

--- js/projects.js
+++ js/projects.js
@@ -1,37 +1,23 @@
+let files = window.location.search.split("=");
+document.getElementById("linkToGitHub").href = `https://github.com/Its-Just-Nans/${files[1]}`;
+document.getElementById("frontTitle").innerHTML = files[1];
 
-var goodData;
-
-let files = window.location.search.split('=');
-document.getElementById('linkToGitHub').href = 'https://github.com/Its-Just-Nans/' + files[1];
-
-try{
-    let client = new HttpClient();
-    client.get('https://raw.githubusercontent.com/Its-Just-Nans/'+files[1]+'/master/README.md', function(response) {
-        let converter = new showdown.Converter();
-        converter.setFlavor('github');
-        let html = converter.makeHtml(response);
-        document.getElementById('projects').innerHTML = html;
-        document.getElementById('frontTitle').innerHTML = files[1];
-        let client2 = new HttpClient();
-        client2.get("https://api.github.com/repos/Its-Just-Nans/" + files[1], function(responseAPI){
-            const apiJSON = JSON.parse(responseAPI);
-            if(apiJSON["has_pages"]){
-                document.getElementById('linkToHTML').href = window.location.origin + '/' + files[1];
-            }else{
-                document.getElementById('linkToHTML').remove();
-            }
-        }, function(error){
-            if(error != "rate limit exceeded"){
-                document.getElementById('linkToHTML').remove();
-            }
-        });        
-    }, function (error){
-        smollPopUp({title : 'Page not found', content : 'You are going to be redirected in 5 sec'}, 'ko');
-        document.getElementById('titleProject').innerHTML = 'It looks like there are no readMe :(';
-        document.getElementById('linkToHTML').remove();
-        setTimeout(function(){window.location = 'https://its-just-nans.github.io/'}, 5000);
+let url = `https://raw.githubusercontent.com/Its-Just-Nans/${files[1]}/master/README.md`;
+renderSpecialObject(url, function (response) {
+    let converter = new showdown.Converter();
+    converter.setFlavor("github");
+    let html = converter.makeHtml(response);
+    document.getElementById("projects").innerHTML = html;
+    document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
     });
-}catch(error){
-    console.log(error)
-}
-//use goodData as JSON
+}, false);
+
+renderSpecialObject(`https://api.github.com/repos/Its-Just-Nans/${files[1]}`, function (data) {
+    const apiJSON = JSON.parse(data);
+    if (apiJSON["has_pages"]) {
+        document.getElementById("linkToHTML").href = `${window.location.origin}/${files[1]}`;
+    } else {
+        document.getElementById("linkToHTML").remove();
+    }
+}, false);
