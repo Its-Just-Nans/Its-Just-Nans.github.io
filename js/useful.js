047761b2927246af147a005aab53bf3a8fe3f0f3
n4n5 <its.just.n4n5@gmail.com>
2022-01-10 21:38:51 +0100

    [fix] clean for faster page

---
+++ js/useful.js
@@ -0,0 +1,17 @@
+function copy(param) {
+    let copieur = document.createElement("textarea");
+    copieur.value = param;
+    copieur.innerHTML = param;
+    copieur.style.width = "1px";
+    copieur.style.height = "1px";
+    document.body.appendChild(copieur);
+    copieur.focus();
+    copieur.select();
+    document.execCommand("copy");
+    try {
+        window.getSelection().removeAllRanges();
+    } catch (e) {
+        console.log("Copy failed");
+    }
+    copieur.remove();
+}
--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,36 @@ import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
 import css from "rollup-plugin-css-only";
+import fs from "fs";
+import path from "path";
+import CleanCSS from 'clean-css';
+import UglifyJS from "uglify-js";
 
 const production = !process.env.ROLLUP_WATCH;
+
+const pathToCSS = path.join(__dirname, "css");
+const pathToJS = path.join(__dirname, "js");
+
+const getCSSandJS = () => {
+    const listOfCSS = fs.readdirSync(pathToCSS);
+    let cssToAdd = "";
+    for (const oneCSS of listOfCSS) {
+        const correctPath = path.join(pathToCSS, oneCSS)
+        cssToAdd += " " + fs.readFileSync(correctPath).toString();
+    }
+    var output = new CleanCSS().minify(cssToAdd);
+    const index = fs.readFileSync("index.template.html").toString();
+    const listOfJS = fs.readdirSync(pathToJS).map((el) => {
+        const a = path.join(pathToJS, el)
+        const js = UglifyJS.minify(fs.readFileSync(a).toString()).code;
+        return js;
+    });
+    let final = index.replace(/\/\/script/, listOfJS.reduce((prev, current) => {
+        return prev + " " + current;
+    }));
+    fs.writeFileSync("index.html", final);
+    return output.styles || "";
+}
 
 export default {
     input: "src/main.js",
@@ -26,7 +54,12 @@ export default {
         }),
         // we'll extract any component CSS out into
         // a separate file - better for performance
-        css({ output: "bundle.css" }),
+        css({
+            output: (styles) => {
+                let output = getCSSandJS();
+                fs.writeFileSync("bundle.css", output + " " + styles);
+            }
+        }),
 
         // If you have external dependencies installed from
         // npm, you'll most likely need these plugins. In
