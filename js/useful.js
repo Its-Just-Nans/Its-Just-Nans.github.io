9139230b6d604e64dd7e7fd9b5064a751e183416
n4n5 <its.just.n4n5@gmail.com>
2022-02-23 20:28:34 +0100

    [fix]

--- js/useful.js
+++ js/useful.js
@@ -1,17 +1,21 @@
 function copy(param) {
-    let copieur = document.createElement("textarea");
-    copieur.value = param;
-    copieur.innerHTML = param;
-    copieur.style.width = "1px";
-    copieur.style.height = "1px";
-    document.body.appendChild(copieur);
-    copieur.focus();
-    copieur.select();
-    document.execCommand("copy");
-    try {
-        window.getSelection().removeAllRanges();
-    } catch (e) {
-        console.log("Copy failed");
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(param)
+    } else {
+        let copieur = document.createElement("textarea");
+        copieur.value = param;
+        copieur.innerHTML = param;
+        copieur.style.width = "1px";
+        copieur.style.height = "1px";
+        document.body.appendChild(copieur);
+        copieur.focus();
+        copieur.select();
+        document.execCommand("copy");
+        try {
+            window.getSelection().removeAllRanges();
+        } catch (e) {
+            console.log("Copy failed");
+        }
+        copieur.remove();
     }
-    copieur.remove();
 }
