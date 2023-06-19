7b52c782164de0219620cad66120db2c411c5cbb
n4n5 <its.just.n4n5@gmail.com>
2023-06-19 14:06:27 +0200

    fix: push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,25 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import remarkToc from "remark-toc";
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://its-just-nans.github.io",
     integrations: [
-        mdx(),
+        svelte(),
+        mdx({
+            remarkPlugins: [remarkMath, remarkToc],
+            rehypePlugins: [rehypeKatex],
+            shikiConfig: {
+                theme: "github-dark",
+                // // Enable word wrap to prevent horizontal scrolling
+                // wrap: true,
+            },
+        }),
         sitemap({
             changefreq: "weekly",
             lastmod: new Date(),
