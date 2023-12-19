cb38e97b279a5a9a93268c0ef753fa1fbc0b4810
n4n5 <its.just.n4n5@gmail.com>
2023-12-19 13:15:17 +0100

    f

---
+++ src/components/Index/links.ts
@@ -0,0 +1,18 @@
+export const links = [
+    {
+        title: "golb",
+        url: "https://its-just-nans.github.io/golb/",
+    },
+    {
+        title: "R&T website",
+        url: "https://its-just-nans.github.io/rt/",
+    },
+    {
+        title: "RIO website",
+        url: "https://its-just-nans.github.io/rio/",
+    },
+    {
+        title: "Packages",
+        url: "https://its-just-nans.github.io/lab/packages",
+    },
+];
