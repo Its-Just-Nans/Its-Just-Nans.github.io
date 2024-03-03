322c758cb277b39eef96258a1386a2c48214ae8a
n4n5 <its.just.n4n5@gmail.com>
2024-03-03 16:59:34 +0100

    better handling of long text

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,7 +1,21 @@
-export const text = (str = "", index = 24) => {
-    str = str || "";
-    return str
-        .substring(0, index)
-        .trim()
-        .concat(str.length > index ? "..." : "");
+export const splitStringByLength = (inputString = "", chunkLength = 10) => {
+    inputString = inputString || "";
+    const words = inputString.split(" ");
+    const result = [];
+
+    let currentChunk = "";
+    for (const word of words) {
+        if ((currentChunk + word).length <= chunkLength) {
+            currentChunk += (currentChunk === "" ? "" : " ") + word;
+        } else {
+            result.push(currentChunk);
+            currentChunk = word;
+        }
+    }
+
+    if (currentChunk !== "") {
+        result.push(currentChunk);
+    }
+
+    return result;
 };
