5c8e22e8c9bb892013a838d7052e1bf2c3e39262
n4n5 <its.just.n4n5@gmail.com>
2024-12-13 23:37:20 +0100

    migrate lab - move to own repo

--- src/Lab/Threlte/store.js
+++
@@ -1,4 +0,0 @@
-import { writable } from "svelte/store";
-
-export const clicked = writable(false);
-export const rigthClicked = writable(false);
--- src/Lab/waldo/waldoData.ts
+++
@@ -1,6 +0,0 @@
-const images = Object.values(import.meta.glob("./*.jpg", { eager: true, query: "?url", import: "default" }));
-
-import data from "./data.json";
-
-export const waldoData = data;
-export const waldoImgs = images;
