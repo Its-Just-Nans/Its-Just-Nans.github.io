a8105f276fc8c542bf961b95723342a950a2de2c
n4n5 <its.just.n4n5@gmail.com>
2025-01-23 15:57:11 +0100

    add new ico

---
+++ src/components/repoData.ts
@@ -0,0 +1,38 @@
+export const allBookmarked = [
+    // finished projects
+    "arduino-led-matrix",
+    "cyberdeck",
+    "flat-hinge",
+    "dojo-snake",
+    "photo-copy",
+    "php-list-api",
+
+    // utility website
+    "lab",
+    "svg-packages-stats",
+    "domains",
+    "bandwidth-calculator",
+    "shared-qr-code",
+    "function-viewer",
+    "json-editor",
+    "ffmpeg-cropper",
+    "readme-compiler",
+    "npm-license-dependencies",
+
+    // knowledge websites
+    "rt",
+    "rio",
+    "golb",
+    "lettre-hebdomadaire.github.io",
+
+    // websites with data
+    "sportiva",
+    "Its-Just-Nans.github.io",
+    "thoughts",
+
+    /// others
+    "dotfiles",
+    "can-be-useful",
+];
+
+export const repoFiltered = ["gitlab-proof", "website-articles", "Its-Just-Nans", "golb-articles"];
