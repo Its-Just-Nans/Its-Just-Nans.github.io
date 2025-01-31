d91b40b123a930e66974f9ea9009d33b9febceaa
n4n5 <its.just.n4n5@gmail.com>
2025-01-31 19:17:56 +0100

    separation of pulls

---
+++ src/components/PullRequests/types.ts
@@ -0,0 +1,27 @@
+export interface PullRequest {
+    node: RepoNode;
+}
+
+export interface RepoNode {
+    id: string;
+    number: number;
+    title: string;
+    url: string;
+    state: string;
+    createdAt: string;
+    baseRepository: {
+        url: string;
+        name: string;
+        description: string;
+        owner: {
+            login: string;
+        };
+        languages: {
+            nodes?:
+                | {
+                      name: string;
+                  }[]
+                | null;
+        };
+    };
+}
