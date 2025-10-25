f10cc32d52810670d88baa39fc286ec44cf08636
n4n5 <its.just.n4n5@gmail.com>
2025-10-25 11:43:34 -0600

    improve repo selectors

--- src/scripts/repoData.ts
+++ src/scripts/repoData.ts
@@ -1,40 +1,6 @@
 export const allBookmarked = [
-    // finished projects
-    "arduino-led-matrix",
-    "cyberdeck",
-    "flat-hinge",
-    "dojo-snake",
+    // projects
     "photo-copy",
-    "php-list-api",
-    "motp",
-
     // utility website
     "lab",
-    "svg-packages-stats",
-    "domains",
-    "bandwidth-calculator",
-    "shared-qr-code",
-    "function-viewer",
-    "json-editor",
-    "ffmpeg-cropper",
-    "readme-compiler",
-    "npm-license-dependencies",
-
-    // knowledge websites
-    "rt",
-    "rio",
-    "golb",
-    "yakb",
-
-    // websites with data
-    "sportiva",
-    "Its-Just-Nans.github.io",
-    "thoughts",
-
-    /// others
-    "dotfiles",
-    "can-be-useful",
-    "lettre-hebdomadaire.github.io",
 ];
-
-export const repoFilter = "🌐";
