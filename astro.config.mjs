a7e2a294b68f2fed6d3d1aa74143d17b1111bac2
n4n5 <its.just.n4n5@gmail.com>
2026-07-20 17:35:42 +0200

    upgrade to astro@v7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,37 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import { unified } from "@astrojs/markdown-remark";
 import sitemap from "@astrojs/sitemap";
 import rehypeExternalLinks from "rehype-external-links";
 import { websiteUrl } from "./src/scripts/pages";
 
 // https://astro.build/config
 export default defineConfig({
-    site: websiteUrl,
-    integrations: [
-        mdx({
-            remarkPlugins: [],
-            rehypePlugins: [[rehypeExternalLinks, { target: "_blank" }]],
-            shikiConfig: {
-                theme: "github-dark",
-                // Enable word wrap to prevent horizontal scrolling
-                // wrap: true,
-            },
-        }),
-        sitemap({
-            changefreq: "weekly",
-            priority: 0.7,
-            lastmod: new Date(),
-            filter: (page) => !page.startsWith(`${websiteUrl}/more/`),
-        }),
-    ],
-    vite: {
-        build: {
-            assetsInlineLimit: 0,
-        },
+  site: websiteUrl,
+  markdown: {
+    processor: unified({
+      remarkPlugins: [],
+      rehypePlugins: [[rehypeExternalLinks, { target: "_blank" }]],
+    }),
+  },
+  integrations: [
+    mdx({
+      shikiConfig: {
+        theme: "github-dark",
+        // Enable word wrap to prevent horizontal scrolling
+        // wrap: true,
+      },
+    }),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+      filter: (page) => !page.startsWith(`${websiteUrl}/more/`),
+    }),
+  ],
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
     },
+  },
 });
