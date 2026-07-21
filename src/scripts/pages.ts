23640b4a85e01b143695364b309234312469dbfc
n4n5 <its.just.n4n5@gmail.com>
2026-07-21 11:50:42 +0200

    add editorconfig

--- src/scripts/pages.ts
+++ src/scripts/pages.ts
@@ -1,20 +1,20 @@
 const nav = [
-  {
-    name: "Home",
-    route: "/",
-  },
-  {
-    name: "Projects",
-    route: "/projects/",
-  },
-  {
-    name: "Articles",
-    route: "/articles/",
-  },
-  {
-    name: "Links",
-    route: "/links/",
-  },
+    {
+        name: "Home",
+        route: "/",
+    },
+    {
+        name: "Projects",
+        route: "/projects/",
+    },
+    {
+        name: "Articles",
+        route: "/articles/",
+    },
+    {
+        name: "Links",
+        route: "/links/",
+    },
 ];
 
 export default nav;
@@ -25,41 +25,49 @@ export const websiteUrl = `https://${website}`;
 
 // external links
 export const githubStats =
-  "https://n4n5.dev/can-be-useful/svelte-github-stats/";
+    "https://n4n5.dev/can-be-useful/svelte-github-stats/";
 
 export const packageLink = "https://packages.n4n5.dev/";
 
 export const CLEANER = [
-  "https://github.com/Its-Just-Nans",
-  "https://github.com/Bel-Art",
+    "https://github.com/Its-Just-Nans",
+    "https://github.com/Bel-Art",
 ];
 
 export const schoolChecker = ["DUT R&T", "TP"];
 
 const hiddenRepos = [
-  "whatisthis",
-  "sms2call",
-  "svelte-number-displayer",
-  "svg-packages-stats",
-  "plugin-astro-content",
-  "go-calendar",
+    "whatisthis",
+    "sms2call",
+    "svelte-number-displayer",
+    "svg-packages-stats",
+    "plugin-astro-content",
+    "go-calendar",
+    // "spust",
+    // "sudoku-solver",
+    "astro-index",
+    "astro-barchart",
+    "astro-simple-carousel",
+    "bladvak",
+    "csv-to-custom-json-python",
+    "static-sitemap",
 ];
 
 export const filterRepo = (
-  name: string,
-  description: string,
-  archivedAt: string | null,
+    name: string,
+    description: string,
+    archivedAt: string | null,
 ) => {
-  if (description.startsWith("🌐")) {
-    console.log(`Project is starting with 🌐: '${name}'`);
-    return false;
-  }
-  if (hiddenRepos.includes(name)) {
-    return false;
-  }
-  if (archivedAt) {
-    console.log(`Project is archived: '${name}'`);
-    return false;
-  }
-  return true;
+    if (description.startsWith("🌐")) {
+        console.log(`Project is starting with 🌐: '${name}'`);
+        return false;
+    }
+    if (hiddenRepos.includes(name)) {
+        return false;
+    }
+    if (archivedAt) {
+        console.log(`Project is archived: '${name}'`);
+        return false;
+    }
+    return true;
 };
